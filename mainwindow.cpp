e2afe6d536f1209909f863194e53bcdce5babde0
ke7omc <ke7omc@gmail.com>
2020-12-31 10:02:35 -0700

    Updated GUI element names.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,14 +17,6 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_listWidget_quickList_itemSelectionChanged()
 {
-    QString fluidNameQString = ui->listWidget_quickList->currentItem()->text();
-
-
-    QString a = ui->listWidget_quickList->currentItem()->text();
-    double internalEnergy = CoolProp::PropsSI("T","P",101325,"Q",0,"Water");
-    std::string internalEnergyString = std::to_string(internalEnergy);
-    QString internalEnergyQString = QString::fromStdString(internalEnergyString);
-    ui->lineEdit->setText(a);
-    ui->lineEdit_3->setText(internalEnergyQString);
+    FluidsNamespace::fluidName = ui->listWidget_quickList->currentItem()->text();
 }
 
